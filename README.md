6bb4919afaf0859130832937d46fb23322586a2f
Daiki48 <daiki48.engineer@gmail.com>
2022-09-14 14:55:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# TTY with crossterm
+
+Just output the size of the terminal.
+
+# Usage
+
+```shell
+$ cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use crossterm::{
+    execute,
+    terminal::{size, SetSize},
+    tty::IsTty,
+};
+
+use std::io::{
+    stdin,
+    stdout,
+};
+
+fn main() {
+    println!{"{:?}", size().unwrap()};
+    execute!(stdout(), SetSize(10, 10)).unwrap();
+    println!("{:?}", size().unwrap());
+
+    if stdin().is_tty() {
+        println!("Is TTY");
+    } else {
+        println!("Is not TTY");
+    }
+}
